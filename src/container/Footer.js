419b7303c311d75778b6ccdb87e75aa39043f881
reijiokito <romeothecong@gmail.com>
2018-09-18 11:57:28 +0700

    Up

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -13,40 +13,21 @@ class Footer extends Component {
                         </div>
                         <hr className="clearfix w-100 d-md-none pb-3" />
                         <div className="col-md-3 mb-md-0 mb-3">
-                            <h5 className="text-uppercase">Links</h5>
+                            <h5 className="text-uppercase">Producers</h5>
 
                             <ul className="list-unstyled">
                                 <li>
-                                    <a href="#!">Link 1</a>
+                                    <a target="_blank" href="https://www.facebook.com/romeothecong">Nguyen The Cong</a>
                                 </li>
                                 <li>
-                                    <a href="#!">Link 2</a>
+                                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100004256869086">Huynh Dang</a>
                                 </li>
                                 <li>
-                                    <a href="#!">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!">Link 4</a>
+                                    <a  target="_blank" href="https://www.facebook.com/pham.tho.94">Pham Tho</a>
                                 </li>
                             </ul>
                         </div>
-                        <div className="col-md-3 mb-md-0 mb-3">
-                            <h5 className="text-uppercase">Links</h5>
-
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="#!">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="#!">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="#!">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!">Link 4</a>
-                                </li>
-                            </ul>
+                        <div className="col-md-3 mb-md-0 mb-3">                            
                         </div>
                     </div>
                 </div>
